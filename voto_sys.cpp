12a651a0dc04892e6fe6445a9aa84e4c8128c5eb
João Vinícius <jvvmg7@gmail.com>
2025-09-03 20:32:59 -0300

    Update voto_sys.cpp
    add func

--- voto_sys.cpp
+++ voto_sys.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+void apurarVotos(int* ptrC1, int* ptrC2, int* ptrC3, int* ptrBrancos, int* ptrNulos, int* ptrTotal);
+void imprimirResultados(int c1, int c2, int c3, int brancos, int nulos, int total);
+
 int main() {
-    int voto = 0;
     int candidato1 = 0;
     int candidato2 = 0;
     int candidato3 = 0;
@@ -19,45 +21,58 @@ int main() {
     std::cout << "-----------------------\n";
     std::cout << "Digite os votos um por um:\n";
 
+    apurarVotos(&candidato1, &candidato2, &candidato3, &brancos, &nulos, &totalEleitores);
+
+    imprimirResultados(candidato1, candidato2, candidato3, brancos, nulos, totalEleitores);
+
+    return 0;
+}
+
+
+void apurarVotos(int* ptrC1, int* ptrC2, int* ptrC3, int* ptrBrancos, int* ptrNulos, int* ptrTotal) {
+    int voto;
+
     while (true) {
         std::cin >> voto;
 
         if (voto == -1) {
-            break;
+            break; 
         }
 
-        totalEleitores++;
+        (*ptrTotal)++; 
 
         switch (voto) {
             case 1:
-                candidato1++;
+                (*ptrC1)++; 
                 break;
             case 2:
-                candidato2++;
+                (*ptrC2)++;
                 break;
             case 3:
-                candidato3++;
+                (*ptrC3)++; 
                 break;
             case 0:
-                brancos++;
+                (*ptrBrancos)++; 
                 break;
             case 4:
-                nulos++;
+                (*ptrNulos)++; 
                 break;
             default:
                 std::cout << "(Voto inválido, computado como nulo)\n";
-                nulos++;
+                (*ptrNulos)++;
                 break;
         }
     }
+}
 
+void imprimirResultados(int c1, int c2, int c3, int brancos, int nulos, int total) {
     std::cout << "\n--- Resultado da Votação ---\n";
 
-    if (candidato1 > candidato2 && candidato1 > candidato3) {
+    if (c1 > c2 && c1 > c3) {
         std::cout << "Candidato vencedor: 1\n";
-    } else if (candidato2 > candidato1 && candidato2 > candidato3) {
+    } else if (c2 > c1 && c2 > c3) {
         std::cout << "Candidato vencedor: 2\n";
-    } else if (candidato3 > candidato1 && candidato3 > candidato2) {
+    } else if (c3 > c1 && c3 > c2) {
         std::cout << "Candidato vencedor: 3\n";
     } else {
         std::cout << "Candidato vencedor: Houve um empate entre os candidatos mais votados.\n";
@@ -65,13 +80,10 @@ int main() {
 
     std::cout << "Número de votos em branco: " << brancos << "\n";
     std::cout << "Número de votos nulos: " << nulos << "\n";
-    std::cout << "Número total de eleitores que votaram: " << totalEleitores << "\n";
+    std::cout << "Número total de eleitores que votaram: " << total << "\n";
     
     std::cout << "\n--- Votos por Candidato ---\n";
-    std::cout << "Candidato 1: " << candidato1 << " votos\n";
-    std::cout << "Candidato 2: " << candidato2 << " votos\n";
-    std::cout << "Candidato 3: " << candidato3 << " votos\n";
-
-
-    return 0;
+    std::cout << "Candidato 1: " << c1 << " votos\n";
+    std::cout << "Candidato 2: " << c2 << " votos\n";
+    std::cout << "Candidato 3: " << c3 << " votos\n";
 }
